e26bfc0a4d8c87eaea2319cbe7545138f98e7a2c
Kamil Sobieraj <sobieraj.k.2@gmail.com>
2019-07-01 20:47:50 +0200

    Update RNPushNotificationListenerService.java

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationListenerService.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationListenerService.java
@@ -30,79 +30,78 @@ public class RNPushNotificationListenerService extends FirebaseMessagingService 
 
     @Override
     public void onMessageReceived(RemoteMessage message) {
-        Log.v(LOG_TAG, "Test: " + Build.VERSION.SDK_INT);
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {
-            Log.v(LOG_TAG, "Version match: " + Build.VERSION.SDK_INT);
-        }
-        String from = message.getFrom();
-        RemoteMessage.Notification remoteNotification = message.getNotification();
+            String from = message.getFrom();
+            RemoteMessage.Notification remoteNotification = message.getNotification();
 
-        final Bundle bundle = new Bundle();
-        // Putting it from remoteNotification first so it can be overriden if message
-        // data has it
-        if (remoteNotification != null) {
-            // ^ It's null when message is from GCM
-            bundle.putString("title", remoteNotification.getTitle());
-            bundle.putString("message", remoteNotification.getBody());
-        }
-
-        for(Map.Entry<String, String> entry : message.getData().entrySet()) {
-            bundle.putString(entry.getKey(), entry.getValue());
-        }
-        JSONObject data = getPushData(bundle.getString("data"));
-        // Copy `twi_body` to `message` to support Twilio
-        if (bundle.containsKey("twi_body")) {
-            bundle.putString("message", bundle.getString("twi_body"));
-        }
-
-        if ((data != null) && (Build.VERSION.SDK_INT > Build.VERSION_CODES.N)) {
-            if (!bundle.containsKey("message")) {
-                bundle.putString("message", data.optString("alert", null));
-            }
-            if (!bundle.containsKey("title")) {
-                bundle.putString("title", data.optString("title", null));
-            }
-            if (!bundle.containsKey("sound")) {
-                bundle.putString("soundName", data.optString("sound", null));
-            }
-            if (!bundle.containsKey("color")) {
-                bundle.putString("color", data.optString("color", null));
+            final Bundle bundle = new Bundle();
+            // Putting it from remoteNotification first so it can be overriden if message
+            // data has it
+            if (remoteNotification != null) {
+                // ^ It's null when message is from GCM
+                bundle.putString("title", remoteNotification.getTitle());
+                bundle.putString("message", remoteNotification.getBody());
             }
 
-            final int badge = data.optInt("badge", -1);
-            if (badge >= 0) {
-                ApplicationBadgeHelper.INSTANCE.setApplicationIconBadgeNumber(this, badge);
+            for(Map.Entry<String, String> entry : message.getData().entrySet()) {
+                bundle.putString(entry.getKey(), entry.getValue());
             }
-        }
+            JSONObject data = getPushData(bundle.getString("data"));
+            // Copy `twi_body` to `message` to support Twilio
+            if (bundle.containsKey("twi_body")) {
+                bundle.putString("message", bundle.getString("twi_body"));
+            }
 
-        Log.v(LOG_TAG, "onMessageReceived: " + bundle);
+            if (data != null) {
+                if (!bundle.containsKey("message")) {
+                    bundle.putString("message", data.optString("alert", null));
+                }
+                if (!bundle.containsKey("title")) {
+                    bundle.putString("title", data.optString("title", null));
+                }
+                if (!bundle.containsKey("sound")) {
+                    bundle.putString("soundName", data.optString("sound", null));
+                }
+                if (!bundle.containsKey("color")) {
+                    bundle.putString("color", data.optString("color", null));
+                }
 
-        // We need to run this on the main thread, as the React code assumes that is true.
-        // Namely, DevServerHelper constructs a Handler() without a Looper, which triggers:
-        // "Can't create handler inside thread that has not called Looper.prepare()"
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
-            public void run() {
-                // Construct and load our normal React JS code bundle
-                ReactInstanceManager mReactInstanceManager = ((ReactApplication) getApplication()).getReactNativeHost().getReactInstanceManager();
-                ReactContext context = mReactInstanceManager.getCurrentReactContext();
-                // If it's constructed, send a notification
-                if (context != null) {
-                    handleRemotePushNotification((ReactApplicationContext) context, bundle);
-                } else {
-                    // Otherwise wait for construction, then send the notification
-                    mReactInstanceManager.addReactInstanceEventListener(new ReactInstanceManager.ReactInstanceEventListener() {
-                        public void onReactContextInitialized(ReactContext context) {
-                            handleRemotePushNotification((ReactApplicationContext) context, bundle);
-                        }
-                    });
-                    if (!mReactInstanceManager.hasStartedCreatingInitialContext()) {
-                        // Construct it in the background
-                        mReactInstanceManager.createReactContextInBackground();
-                    }
+                final int badge = data.optInt("badge", -1);
+                if (badge >= 0) {
+                    ApplicationBadgeHelper.INSTANCE.setApplicationIconBadgeNumber(this, badge);
                 }
             }
-        });
+
+            Log.v(LOG_TAG, "onMessageReceived: " + bundle);
+
+            // We need to run this on the main thread, as the React code assumes that is true.
+            // Namely, DevServerHelper constructs a Handler() without a Looper, which triggers:
+            // "Can't create handler inside thread that has not called Looper.prepare()"
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                public void run() {
+                    // Construct and load our normal React JS code bundle
+                    ReactInstanceManager mReactInstanceManager = ((ReactApplication) getApplication()).getReactNativeHost().getReactInstanceManager();
+                    ReactContext context = mReactInstanceManager.getCurrentReactContext();
+                    // If it's constructed, send a notification
+                    if (context != null) {
+                        handleRemotePushNotification((ReactApplicationContext) context, bundle);
+                    } else {
+                        // Otherwise wait for construction, then send the notification
+                        mReactInstanceManager.addReactInstanceEventListener(new ReactInstanceManager.ReactInstanceEventListener() {
+                            public void onReactContextInitialized(ReactContext context) {
+                                handleRemotePushNotification((ReactApplicationContext) context, bundle);
+                            }
+                        });
+                        if (!mReactInstanceManager.hasStartedCreatingInitialContext()) {
+                            // Construct it in the background
+                            mReactInstanceManager.createReactContextInBackground();
+                        }
+                    }
+                }
+            });
+        }
+        
     }
 
     private JSONObject getPushData(String dataString) {
